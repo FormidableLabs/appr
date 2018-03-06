6f3d60896cab022d84d663149447b110c1af7d85
JP Driver <jp.driver@formidable.com>
2018-03-06 16:56:33 +0000

    remove githubPullRequestId from env var validation

--- scripts/post-deploy.js
+++ scripts/post-deploy.js
@@ -6,10 +6,8 @@ module.exports = function postDeploy() {
   const expUrl = `https://expo.io/@${config.expUsername}/${utils.readPackageJSON().name}`;
   const expUrlForQRCode = `https://exp.host/@${config.expUsername}/${utils.readPackageJSON().name}`;
   const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${expUrlForQRCode}`;
-  const issueUrl = `https://${config.githubUsername}:${config.githubToken}@api.github.com/repos/${config.githubOrg}/${config.githubRepo}/issues/${config.githubPullRequestId}/comments`;
 
   log('Exponent URL', expUrl);
-  log('GitHub Issue URL', issueUrl);
   log('QR Code URL ', qrUrl);
 
   const body = `
@@ -21,20 +19,24 @@ module.exports = function postDeploy() {
   ![QR Code](${qrUrl})
   `;
 
-  request.post(
-    {
-      url: issueUrl,
-      headers: { 'User-Agent': 'ci' },
-      body: JSON.stringify({ body })
-    },
-    (error, response) => {
-      if (error) {
-        console.error('Failed to post comment to GitHub, an error occurred', error);
-      } else if (response.statusCode >= 400) {
-        console.error('Failed to post comment to GitHub, request failed with', response);
-      } else {
-        console.log(`Posted message to GitHub PR #${config.githubPullRequestId}`);
+  if (config.githubPullRequestId) {
+    const issueUrl = `https://${config.githubUsername}:${config.githubToken}@api.github.com/repos/${config.githubOrg}/${config.githubRepo}/issues/${config.githubPullRequestId}/comments`;
+    log('GitHub Issue URL', issueUrl);
+    request.post(
+      {
+        url: issueUrl,
+        headers: { 'User-Agent': 'ci' },
+        body: JSON.stringify({ body })
+      },
+      (error, response) => {
+        if (error) {
+          console.error('Failed to post comment to GitHub, an error occurred', error);
+        } else if (response.statusCode >= 400) {
+          console.error('Failed to post comment to GitHub, request failed with', response);
+        } else {
+          console.log(`Posted message to GitHub PR #${config.githubPullRequestId}`);
+        }
       }
-    }
-  );
+    );
+  }
 };
